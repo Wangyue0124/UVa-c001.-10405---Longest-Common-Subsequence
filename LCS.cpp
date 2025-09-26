d0c67f90b30170844d9e8774b2727e8c588821c0
Wangyue0124 <diego960124@gmail.com>
2025-09-27 00:02:39 +0800

    Add files via upload

---
+++ LCS.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	string x,y;
+	while (cin >> x, cin >> y) //輸入
+	{
+		int n = x.size(), m = y.size();
+		vector<vector<int>>dp(n + 1, vector<int>(m + 1));
+		vector<char>ans;
+		//LCS長度
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= m; j++)
+			{
+				if (x[i - 1] == y[j - 1])
+				{
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				}
+				else
+				{
+					dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		//回朔
+		while (n && m)
+		{
+			if (dp[n][m-1] == dp[n - 1][m] && dp[n][m] > dp[n-1][m - 1])
+			{
+				ans.push_back(x[n - 1]);
+				n--;
+				m--;
+			}
+			else if (dp[n][m] == dp[n - 1][m])
+			{
+				n--;
+			}
+			else
+			{
+				m--;
+			}
+		}
+		//輸出
+		int l = ans.size();
+		cout << l << endl;
+	}
+	return 0;
+}
